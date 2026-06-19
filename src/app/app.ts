7be97909d45ef429f1a9ebb53e02357db508796b
Sneh Kurhade <sneh.kurhade0@gmail.com>
2026-06-19 12:10:59 +0530

    navbar modernization

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import { HostListener } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  
+isMenuOpen = false;
+currentYear = new Date().getFullYear();
+
+  @HostListener('window:resize')
+  onResize() {
+    if (window.innerWidth > 768) {
+      this.isMenuOpen = false;
+    }
+  }
+}
